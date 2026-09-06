ed9551d7917bdf879f4e9213a806bb004b059595
rayrayroadster <73283089+rayrayroadster@users.noreply.github.com>
2026-09-06 23:10:45 +0800

    New Menu Screen
    Refreshed PLAY+ QUIT button design and UI

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Create Main menu button function
+    public void PlayGame()
+    {   //Load Main Game when PLAY button is pressed
+        SceneManager.LoadSceneAsync("Main Game");
+    }
+
+    //Create Quit button function
+    public void QuitGame()
+    {
+        //Quits game when QUIT button is pressed
+        Application.Quit();
+    }
+}
