2247a4ec071d5f16c36d20d02529a63e45b6daaf
xPrinceV <princeweegaming@gmail.com>
2026-09-09 00:11:39 +0800

    WIP Titan Enemy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,9 +21,11 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         //Temporary for now until weapon chest implemented
-        AddWeapon(4);
-        // AddWeapon(0);
-        // AddWeapon(0);
+        AddWeapon(0);
+        AddWeapon(0);
+        AddWeapon(0);
+        AddWeapon(0);
+        AddWeapon(0);
     }
 
     // Update is called once per frame
