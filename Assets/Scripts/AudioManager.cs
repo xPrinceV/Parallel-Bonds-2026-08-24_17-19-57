f6313fdf64b673ecb9daefc7916311d7acfc68bf
rayrayroadster <73283089+rayrayroadster@users.noreply.github.com>
2026-09-14 12:26:24 +0800

    Revert "Merge pull request #6 from xPrinceV/revert-5-WIP2-Ray-merge"
    This reverts commit 402fc0f032f21aaed5894b75d3c8c02e38f4d718, reversing
    changes made to f9b2a408caab9dceaaafdcd442883712c2091b8d.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header ("--Audio Source--")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("--BGM--")]
+    public AudioClip realworld;
+    public AudioClip ruinedworld;
+    public AudioClip bossmusic;
+    public AudioClip victory;
+
+    [Header("--Weapons SFX--")]
+    public AudioClip jeffPistol;
+    public AudioClip lantern;
+    public AudioClip lightning;
+    public AudioClip bow;
+    public AudioClip aura;
+    public AudioClip sniper;
+    public AudioClip dagger;
+    public AudioClip scythe;
+
+    //Play realworld BGM at start of game
+    private void Start()
+    {
+        musicSource.clip = realworld;
+        musicSource.Play();
+    }
+
+    //Play SFX
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
