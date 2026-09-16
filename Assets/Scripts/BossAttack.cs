6836e9ec35442541c47574ce075a961c01ff2cca
Jess <jess014323@gmail.com>
2026-09-17 00:47:25 +0800

    Screen frames

---
+++ Assets/Scripts/BossAttack.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+
+public class BossAttack : MonoBehaviour
+{
+
+    public void OrbAttack()
+    {
+        Debug.Log("Orb Attack");
+    }
+
+    public void LightningAttack()
+    {
+        Debug.Log("Lightning Attack");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
